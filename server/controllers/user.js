63398e88ce61a98c56397b086bbee00f5ce1af1d
bertash <y.bertash@gmail.com>
2021-10-05 09:51:02 +0300

    fix: change error language

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,66 +1,81 @@
-const {userRegistration, activateUser, loginUser, logoutUser, refreshUserToken} = require("../service/user");
-const {validationResult} = require('express-validator');
+const {
+  userRegistration,
+  activateUser,
+  loginUser,
+  logoutUser,
+  refreshUserToken,
+} = require('../service/user');
+const { validationResult } = require('express-validator');
 const ApiError = require('../exceptions/api-error');
 
-const registration = async(req,res) => {
+const registration = async (req, res) => {
   try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return next(ApiError.BadRequest('Ошибка при валидации', errors.array()))
+      return next(ApiError.BadRequest('Validation error!', errors.array()));
     }
 
-    const {email, password} = req.body;
+    const { email, password } = req.body;
     const userData = await userRegistration(email, password);
-    res.cookie("refreshToken", userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 *1000, httpOnly: true});
+    res.cookie('refreshToken', userData.refreshToken, {
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+    });
 
     return res.json(userData);
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
-const login = async(req,res,next) => {
+const login = async (req, res, next) => {
   try {
-    const {email, password} = req.body;
+    const { email, password } = req.body;
     const userData = await loginUser(email, password);
-    res.cookie("refreshToken", userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 *1000, httpOnly: true});
+    res.cookie('refreshToken', userData.refreshToken, {
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+    });
     return res.json(userData);
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
-const logout = async(req,res,next) => {
+const logout = async (req, res, next) => {
   try {
-    const {refreshToken} = req.cookies;
+    const { refreshToken } = req.cookies;
     const token = await logoutUser(refreshToken);
-    res.clearCookie("refreshToken");
+    res.clearCookie('refreshToken');
     return res.json(token);
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
-const activate = async(req,res,next) => {
+const activate = async (req, res, next) => {
   try {
-     const activationLink = req.params.link;
-     await activateUser(activationLink);
-     return res.redirect(process.env.CLIENT_URL);
+    const activationLink = req.params.link;
+    await activateUser(activationLink);
+    return res.redirect(process.env.CLIENT_URL);
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
-const refresh = async(req,res,next) => {
+const refresh = async (req, res, next) => {
   try {
-    const {refreshToken} = req.cookies;
+    const { refreshToken } = req.cookies;
     const userData = await refreshUserToken(refreshToken);
-    res.cookie("refreshToken", userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 *1000, httpOnly: true});
+    res.cookie('refreshToken', userData.refreshToken, {
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+    });
     return res.json(userData);
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 exports.registration = registration;
 exports.activate = activate;
