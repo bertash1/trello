a41907889b90d8f787c33865a92940025f11f5f1
Yauheni Bertash <Yauheni.Bertash@itechart-group.com>
2021-10-14 11:56:07 +0300

    feat: add member bar to header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import React from "react"
+import MemberBar from "./MemberBar/MemberBar"
 import "./style.sass"
 
 const Header = () => (
   <header className="header">
     <span className="header__logo">Custom Trello</span>
+    <MemberBar />
   </header>
 )
 
