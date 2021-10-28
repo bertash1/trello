c5c22332b6269244f905a518b438a138ce099e87
Yauheni Bertash <Yauheni.Bertash@itechart-group.com>
2021-10-28 13:27:23 +0300

    refactor: delete comments

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,16 +1,15 @@
 import React, { useEffect } from "react"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import PropTypes from "prop-types"
 
 import { getUserBoards } from "../../actions/board"
 import Header from "../../components/Header/Header"
 import "./style.sass"
-// import Board from "../../components/Board/Board"
-import ActiveBoard from "../ActiveBoard/ActiveBoard"
+import Board from "../../components/Board/Board"
 
 const Dashboard = ({ userId }) => {
   const dispatch = useDispatch()
-  // const { userBoards = [] } = useSelector((state) => state.boards)
+  const { userBoards = [] } = useSelector((state) => state.boards)
 
   useEffect(() => {
     dispatch(getUserBoards(userId))
@@ -19,14 +18,13 @@ const Dashboard = ({ userId }) => {
   return (
     <div className="dashboard__wrapper">
       <Header />
-      {/* <h3 className="dashboard__title">Your boards</h3>
+      <h3 className="dashboard__title">Your boards</h3>
       <div className="boards-wrapper">
         {userBoards &&
           userBoards.map((item) => (
             <Board key={item._id} title={item.title} boardId={item._id} />
           ))}
-      </div> */}
-      <ActiveBoard />
+      </div>
     </div>
   )
 }
